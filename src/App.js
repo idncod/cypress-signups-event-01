19381c49a9768193031a59843eee9bdf43ec7ee8
idncod <idncod@protonmail.com>
2024-02-11 06:52:35 +0000

    v_01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { db } from './firebase';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 
 function App() {
+
+const [input, setInput] = useState(""); 
+
+const inputHandler = (e) =>
+{
+  setInput(e.target.value);
+}
+
+
+const submitHandler = async (e) =>
+{
+  e.preventDefault();
+
+  if(input)
+  {
+    await addDoc(collection(db, "emails"),
+      {
+        email: input,
+        timestamp: serverTimestamp(),
+      })
+      setInput("");
+  }
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="emails">
+      <form onSubmit={submitHandler} className="form">
+        <p>Register to attend a session wit a Cypress representative</p>
+        <input className="email-input" onChange={inputHandler} placeholder="Email" type="email" value={input}></input>
+        <button className="email-button">Count me in!</button>
+      </form>
     </div>
   );
 }

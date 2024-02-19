6b2ed07d407e0072a4d354772000c537c2a896a9
idncod <idncod@protonmail.com>
2024-02-19 16:37:47 +0000

    security patch 03.1

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyAhuMmosZUHA9_EpZI7byodN6bEG5wm3dA",
+  authDomain: "cyp-su.firebaseapp.com",
+  projectId: "cyp-su",
+  storageBucket: "cyp-su.appspot.com",
+  messagingSenderId: "498100333883",
+  appId: "1:498100333883:web:24e0da7d636a61dd1edb1e"
+
 };
 
 const app = initializeApp(firebaseConfig);
